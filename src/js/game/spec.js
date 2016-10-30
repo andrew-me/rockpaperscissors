28a3b71765651473f54fb5bc0f650fa40d08a673
Andrew Edwards <andrew@ethicalstudios.com>
2016-10-30 18:49:24 +0000

    Add game spec

--- src/js/game/spec.js
+++ src/js/game/spec.js
@@ -1,7 +1,7 @@
 /* eslint-env mocha */
 /* global expect:false */
 
-import { iterate, reset } from './use-cases';
+import { iterate, reset, getState, addPlayer, addWeapons } from './use-cases';
 
 describe('game', () => {
 
@@ -12,26 +12,54 @@ describe('game', () => {
 	});
 
   it('should reset', () => {
-    const origGameData = makeDummyGameData();
-    const gameData = makeDummyGameData();
-    gameData.target = 6;
-    gameData.iteration = 3;
+    let origGameData = makeDummyGameData();
+    origGameData = addPlayer(origGameData, 0);
+    origGameData = addPlayer(origGameData, 1);
+
+    let gameData = makeDummyGameData();
+    gameData = addPlayer(gameData, 0);
+    gameData = addPlayer(gameData, 1);
+    gameData.target = origGameData.target + 1;
+    gameData.iteration = origGameData.target - 2;
     gameData.players[0].score = 2;
     gameData.players[1].score = 0;
+
     const resetGameData = reset(gameData);
     expect(resetGameData).to.deep.equal(origGameData);
 	});
 
   it('should indicate game state', () => {
+    const gameDataStart = makeDummyGameData();
+    const stateStart = getState(gameDataStart);
+    expect(stateStart).to.equal('start');
 
+    let gameDataPlaying = makeDummyGameData();
+    gameDataPlaying.iteration = 1;
+    const statePlaying = getState(gameDataPlaying);
+    expect(statePlaying).to.equal('playing');
+
+    let gameDataEnd = makeDummyGameData();
+    gameDataEnd.iteration = gameDataEnd.target;
+    const stateEnd = getState(gameDataEnd);
+    expect(stateEnd).to.equal('end');
 	});
 
-  it('should add a player', () => {
+  it('should add players', () => {
+    let gameData = makeDummyGameData();
 
+    gameData = addPlayer(gameData, 0);
+    expect(gameData.players[0].id).to.equal(0);
+
+    gameData = addPlayer(gameData, 1);
+    expect(gameData.players[1].id).to.equal(1);
 	});
 
   it('should add weapons', () => {
-
+    const gameData = makeDummyGameData();
+    const weapons = makeDummyWeapons();
+    const withWeapons = addWeapons(gameData, weapons);
+    expect(withWeapons.weapons.items[0].id === weapons.items[0].id);
+    expect(withWeapons.weapons.items.length === weapons.items.length);
 	});
 
   it('should play a turn', () => {
@@ -48,15 +76,39 @@ function makeDummyGameData() {
   const game = {
     target: 5,
     iteration: 0,
-    players: [{
-      id: null,
-      score: 0
-    }, {
-      id: null,
-      score: 0
-    }],
+    players: [],
     weapons: null
   };
 
 	return game;
+}
+
+function makeDummyWeapons() {
+  const weapons = {
+    getAllWeapons: function(){},
+    items: [{
+      id: 0,
+      name: 'Paper',
+      beats: [{
+        id: 2,
+        message: 'Paper wraps Stone'
+      }]
+    }, {
+      id: 1,
+      name: 'Scissors',
+      beats: [{
+        id: 0,
+        message: 'Scissors cut Paper'
+      }]
+    }, {
+      id: 2,
+      name: 'Stone',
+      beats: [{
+        id: 1,
+        message: 'Stone blunts Scissors'
+      }]
+    }]
+  };
+
+	return weapons;
 }
--- src/js/game/use-cases.js
+++ src/js/game/use-cases.js
@@ -1,14 +1,19 @@
 export {
   iterate,
-  reset
+  reset,
+  getState,
+  addPlayer,
+  addWeapons
 };
 
 function iterate(game) {
+  validateGame(game);
   game.iteration++;
   return game;
 }
 
 function reset(game) {
+  validateGame(game);
   game.target = 5;
   game.iteration = 0;
   game.players[0].score = 0;
@@ -16,16 +21,39 @@ function reset(game) {
   return game;
 }
 
+function getState(game) {
+  validateGame(game);
+  if(game.iteration === 0) return 'start';
+  if(game.iteration === game.target) return 'end';
+  return 'playing';
+}
 
-export const data = {
-  target: 5,
-  iteration: 0,
-  players: [{
-    id: null,
-    score: 0
-  }, {
-    id: null,
-    score: 0
-  }],
-  weapons: null
-};
+function addPlayer(game, id) {
+  validateGame(game);
+  game.players.push({id: id, score: 0});
+  return game;
+}
+
+function addWeapons(game, weapons) {
+  validateGame(game);
+  validateWeapons(weapons);
+  game.weapons = weapons;
+  return game;
+}
+
+function validateGame(game) {
+  if (!(game !== null && typeof game === 'object') ||
+      (('id' in game) &&
+      !(typeof game.id === 'number'))) {
+    throw new Error('invalid game object');
+  }
+}
+
+function validateWeapons(weapons) {
+  if (!(weapons !== null && typeof weapons === 'object') ||
+      !(typeof(weapons.getAllWeapons) === typeof(Function)) ||
+      ('items' in weapons) &&
+      !(Array.isArray(weapons.items))) {
+    throw new Error('invalid weapons object');
+  }
+}
