77052759f4b40fe541aa0405d6a6d2eb846def9a
Andrew Edwards <andrew@ethicalstudios.com>
2016-10-30 21:41:58 +0000

    Add weapons spec

--- src/js/weapons/spec.js
+++ src/js/weapons/spec.js
@@ -1,27 +1,54 @@
 /* eslint-env mocha */
 /* global expect:false */
 
-import { getAllWeapons } from './use-cases';
+import { getWeapons, getAllWeaponsData, getRandomWeapon, filterWeaponsById, declareWinner } from './use-cases';
 
 describe('weapons', () => {
 
-  it('should return all weapons', () => {
+  it('should return a weapons object', () => {
     const weaponsData = makeDummyWeaponsData();
-    const allWeapons = getAllWeapons(weaponsData);
+    const weapons = getWeapons(weaponsData);
+    expect(typeof(weapons.getAllWeaponsData)).to.equal(typeof(Function));
+    expect(weapons.items[0].id === weaponsData[0].id);
+	});
+
+  it('should return all weapons data', () => {
+    const weaponsData = makeDummyWeaponsData();
+    const allWeapons = getAllWeaponsData(weaponsData);
     expect(allWeapons.length).to.equal(weaponsData.length);
     expect(allWeapons[0].id).to.equal(weaponsData[0].id);
 	});
 
   it('should return random weapon', () => {
-
+    const weaponsData = makeDummyWeaponsData();
+    const weapon = getRandomWeapon(weaponsData);
+    expect(weapon.id).to.be.gte(0);
 	});
 
   it('should filter weapons by id', () => {
-
+    const weaponsData = makeDummyWeaponsData();
+    const weapon = filterWeaponsById(weaponsData, weaponsData[0].id);
+    expect(weapon.id).to.equal(weaponsData[0].id);
 	});
 
   it('should declare winning weapon', () => {
+    const weaponsData = makeDummyWeaponsData();
+    const winner1 = declareWinner(weaponsData, 0, 1);
+    const winner2 = declareWinner(weaponsData, 1, 2);
+    const winner3 = declareWinner(weaponsData, 2, 0);
+    const draw = declareWinner(weaponsData, 0, 0);
 
+    expect(winner1.id).to.equal(1);
+    expect(winner1.message).to.equal('Scissors cut Paper');
+
+    expect(winner2.id).to.equal(2);
+    expect(winner2.message).to.equal('Stone blunts Scissors');
+
+    expect(winner3.id).to.equal(0);
+    expect(winner3.message).to.equal('Paper wraps Stone');
+
+    expect(draw.id).to.equal(-1);
+    expect(draw.message).to.equal('Draw');
 	});
 
 });
--- src/js/weapons/use-cases.js
+++ src/js/weapons/use-cases.js
@@ -1,10 +1,56 @@
 export {
-  getAllWeapons
+  getWeapons,
+  getAllWeaponsData,
+  getRandomWeapon,
+  filterWeaponsById,
+  declareWinner
 };
 
-function getAllWeapons(weapons) {
+function getWeapons(weapons) {
+  validateWeapons(weapons);
+  return {
+    getAllWeaponsData: getAllWeaponsData,
+    items: weapons
+  }
+}
+
+function getAllWeaponsData(weapons) {
   validateWeapons(weapons);
   return weapons;
+}
+
+function getRandomWeapon(weapons) {
+  validateWeapons(weapons);
+  const weapon = weapons[Math.floor(Math.random()*weapons.length)];
+  return weapon;
+}
+
+function filterWeaponsById(weapons, id) {
+  validateWeapons(weapons);
+  const filteredWeapons = weapons.filter(weapon => weapon.id === id);
+  return filteredWeapons[0];
+}
+
+function declareWinner(weapons, id1, id2) {
+  if(id1 === id2) return {
+    id: -1,
+    message: 'Draw'
+  }
+
+  validateWeapons(weapons);
+  const weapon1 = filterWeaponsById(weapons, id1);
+  const weapon2 = filterWeaponsById(weapons, id2);
+
+  const checkWeapon1 = weapon1.beats.filter(beat => beat.id === id2);
+  if(checkWeapon1.length) return {id:id1, message: checkWeapon1[0].message};
+
+  const checkWeapon2 = weapon2.beats.filter(beat => beat.id === id1);
+  if(checkWeapon2.length) return {id:id2, message: checkWeapon2[0].message};
+
+  return {
+    id: -1,
+    message: 'Draw'
+  }
 }
 
 function validateWeapons(weapons) {
