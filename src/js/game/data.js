534b8ef7848908b01c6c6dfcf6312a9f3a819e63
Andrew Edwards <andrew@ethicalstudios.com>
2016-10-31 12:13:44 +0000

    Remove players from starting game data
    They're added on init, so no need

--- src/js/game/data.js
+++ src/js/game/data.js
@@ -1,13 +1,7 @@
 export const data = {
   target: 5,
   iteration: 0,
-  players: [{
-    id: null,
-    score: 0
-  }, {
-    id: null,
-    score: 0
-  }],
+  players: [],
   weapons: null,
   message: null
 };
