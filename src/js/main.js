7a1e9941f8468fdc24d22a5f4e5b85852a9a515a
Andrew Edwards <andrew@ethicalstudios.com>
2016-10-31 10:20:41 +0000

    Init users and weapons

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,9 @@
 require('../css/main.scss');
 
-import { data, getHumanUser, getBotUser, filterUsersById } from './users';
-import { data, getWeapons } from './weapons';
-import { data, reset, getState, addPlayer, addWeapons, play, checkIfWinner } from './game';
+import { userData, getHumanUser, getBotUser, filterUsersById } from './users';
+import { weaponsData, getWeapons } from './weapons';
+import { gameData, reset, getState, addPlayer, addWeapons, play, checkIfWinner } from './game';
+
+const humanUser = getHumanUser(userData);
+const botUser = getBotUser(userData);
+const weapons = getWeapons(weaponsData);
