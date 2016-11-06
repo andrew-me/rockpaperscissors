14b93db23411d3afe19121e8f71e658e9df2a0e2
Andrew Edwards <andrew@ethicalstudios.com>
2016-11-06 21:28:45 +0000

    Display bot user for the human to switch to

--- src/js/users/template.js
+++ src/js/users/template.js
@@ -3,13 +3,17 @@ export {
 };
 
 function renderUser(user, score, altUsers) {
+  let altUsersHTML = '';
+  if(altUsers){
+    altUsersHTML = altUsers.map((altUser) => `<li>${altUser.name}</li>`);
+    altUsersHTML = `<details><summary>Switch User</summary><ul>${altUsersHTML}</ul></details>`;
+  }
+
   const div = document.createElement('div');
   div.className = 'user';
   div.innerHTML =  (`
     <h2 class="user__name">${user.name} <span class="user__type">(${user.type})</span></h2>
-    <details>
-      <summary>Switch User</summary>
-    </details>
+    ${altUsersHTML}
     <p class="user__score">score: ${score}</p>
   `);
 
