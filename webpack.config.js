2f2bd7325fe7bae2559609bbc18e7b6906b267ea
Andrew Edwards <andrew@ethicalstudios.com>
2016-11-09 22:51:02 +0000

    Add web pack image loader

--- webpack.config.js
+++ webpack.config.js
@@ -48,6 +48,12 @@ module.exports = {
 			test: /\.scss$/,
 			loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')
 		},{
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      loaders: [
+        'file?hash=sha512&digest=hex&name=[hash].[ext]',
+        'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+      ]
+    },{
 			test: /\.js$/,
 			exclude: [/node_modules/],
 			loader: 'babel'
