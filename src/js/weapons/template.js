70a58416f66ca7d3e7dafa7e77cf11e9cd351e53
Andrew Edwards <andrew@ethicalstudios.com>
2016-11-13 11:07:18 +0000

    Weapon images & styles

--- src/js/weapons/template.js
+++ src/js/weapons/template.js
@@ -1,3 +1,5 @@
+require('./styles.scss');
+
 export {
   renderWeapon,
   renderWeapons
@@ -6,13 +8,20 @@ export {
 function renderWeapon(weapon, handler) {
   if(!weapon) return;
 
+  let classes = 'weapon__name ';
+
   const h2 = document.createElement('h2');
   if(handler){
     h2.onclick = handler(weapon.id);
+    classes += 'weapon__name--choice ';
   }
 
-  h2.className = 'weapon__name';
-  h2.innerHTML = weapon.name;
+  h2.className = classes + 'weapon__name--' + weapon.name.toLowerCase();
+
+  const span = document.createElement('span');
+  span.className = 'hide';
+  span.innerHTML = weapon.name;
+  h2.appendChild(span);
 
   const div = document.createElement('div');
   div.className = 'weapon';
@@ -26,6 +35,7 @@ function renderWeapons(weapons, handler) {
   ul.className = 'weapons';
   weapons.forEach((weapon) => {
     let li = document.createElement('li');
+    li.className = 'weapon__item';
     let div = renderWeapon(weapon, handler);
     li.appendChild(div);
     ul.appendChild(li);
